e1933f37255a51c5367e2c9401880ff48ea313d4
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-21 01:11:34 +0100

    added prisma

--- backend/api.js
+++ backend/api.js
@@ -2,4 +2,20 @@ const express = require("express")
 
 const app = express()
 
-app.listen(3000)
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+app.get('/tasks-get', async (req, res) => {
+    try {
+      const tasks = await prisma.task.findMany();
+      res.json(tasks);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
+  
+  
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
